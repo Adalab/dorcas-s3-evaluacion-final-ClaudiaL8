1be2af93d16e71f728bd73294aa01e36d8523f19
Claudia <cl.calerolopez@gmail.com>
2018-08-08 08:36:22 +0200

    peticion fetch

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-    
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+    this.personajes = this.personajes.bind(this);
+    this.personajes()
+  }
+
+  personajes() {
+    fetch(
+      'http://hp-api.herokuapp.com/api/characters'
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then((json) => {
+        const persons = json;
+        this.setState({
+          data: persons
+        })
+      })
+  }
   render() {
     return (
       <div className="App">
-
       </div>
     );
   }
